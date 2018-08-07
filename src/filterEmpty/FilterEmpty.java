ab43b926767d3bee5e9a51d1a762ff3c55ac8b9f
Ryan Buchmayer <buchmr@uw.edu>
2018-08-06 19:55:49 -0700

    Filter almost done

--- src/filterEmpty/FilterEmpty.java
+++ src/filterEmpty/FilterEmpty.java
@@ -3,57 +3,153 @@ package filterEmpty;
 import java.util.Arrays;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
 
 import cse332.exceptions.NotYetImplementedException;
 
 public class FilterEmpty {
-    static ForkJoinPool POOL = new ForkJoinPool();
+	static ForkJoinPool POOL = new ForkJoinPool();
+	public static int SUM = 0;
 
-    public static int[] filterEmpty(String[] arr) {
-        int[] bitset = mapToBitSet(arr);
-        //System.out.println(java.util.Arrays.toString(bitset));
-        int[] bitsum = ParallelPrefixSum.parallelPrefixSum(bitset);
-        //System.out.println(java.util.Arrays.toString(bitsum));
-        int[] result = mapToOutput(arr, bitsum);
-        return result;
-    }
+	public static int[] filterEmpty(String[] arr) {
+		int[] bitset = mapToBitSet(arr);
+		
+		int[] bitsum = ParallelPrefixSum.parallelPrefixSum(bitset);
+		int[] result = mapToOutput(arr, bitsum);
+		return result;
+	}
 
-    public static int[] mapToBitSet(String[] arr) {
-        int[] bits = new int[arr.length];
-        for(int i = 0; i < arr.length; i++) {
-        	if(arr[i].length() > 0) {
-        		bits[i] = 1;
-        	}
-        }
-        return ParallelPrefixSum.parallelPrefixSum(bits);
-    }
+	public static int[] mapToBitSet(String[] arr) {
 
-    
-    public static int[] mapToOutput(String[] input, int[] bitsum) {
-    	int last = bitsum[bitsum.length - 1];
-        int[] output = new int[last];1111
-        int j = 0;
-        int sum = 0;
-        for(int i = 0; i < input.length; i++) {
-        	if(bitsum[i] != sum) {
-        		output[j] = input[i].length();
-        		sum += bitsum[i];
-        	}
-        }
-        return output;
-    }
+		class SumThread extends RecursiveTask<int[]> {
+			private static final long serialVersionUID = 1L;
+			int lo;
+			int hi;
+			String[] arr;
 
-    private static void usage() {
-        System.err.println("USAGE: FilterEmpty <String array>");
-        System.exit(1);
-    }
+			public SumThread(int l, int h, String[] a) {
+				lo = l;
+				hi = h;
+				arr = a;
+			}
 
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            usage();
-        }
+			protected int[] compute() {
+				if (hi - lo <= 1) {
+					int[] bits = new int[hi - lo];
+					for (int i = lo; i < hi; i++) {
+						if (arr[i].length() > 0) {
+							bits[0] = 1;
+						}
+					}
+					return bits;
+				}
 
-        String[] arr = args[0].replaceAll("\\s*", "").split(",");
-        System.out.println(Arrays.toString(filterEmpty(arr)));
-    }
+				else {
+					SumThread left = new SumThread(lo, (hi + lo) / 2, arr);
+					SumThread right = new SumThread((hi + lo) / 2, hi, arr);
+					left.fork();
+					int[] rightAns = right.compute();
+					int[] leftAns = left.join();
+					int[] out = new int[rightAns.length + leftAns.length];
+					for (int i = 0; i < leftAns.length; i++) {
+						out[i] = leftAns[i];
+					}
+					int j = 0;
+					for (int i = leftAns.length; i < out.length; i++) {
+						out[i] = rightAns[j];
+						j++;
+					}
+					return out;
+				}
+
+			}
+
+		}
+		SumThread thd = new SumThread(0, arr.length, arr);
+		return ForkJoinPool.commonPool().invoke(thd);
+
+	}
+
+	public static int[] mapToOutput(String[] input, int[] bitsum) {
+		class SumThread extends RecursiveTask<int[]> {
+			private static final long serialVersionUID = 1L;
+			int lo;
+			int hi;
+			String[] in;
+			int[] bitsum;
+			
+
+			public SumThread(int l, int h, String[] input, int[] bitsum) {
+				lo = l;
+				hi = h;
+				in = input;
+				this.bitsum = bitsum;
+				
+			}
+
+			protected int[] compute() {
+
+				if (hi - lo <= 1) {
+
+					int[] output = new int[hi - lo];
+
+					int j = 0;
+					for (int i = lo; i < hi; i++) {
+						if (bitsum[i] == 0 || bitsum[i] <= SUM) {
+							return new int[0];
+						}
+
+						if(bitsum[i] > SUM) {
+							SUM = bitsum[i];
+							output[j] = in[i].length();
+							j++;
+						}
+						
+
+					}
+
+					return output;
+
+				}
+
+				else {
+					SumThread left = new SumThread(lo, (hi + lo) / 2, in, bitsum);
+					SumThread right = new SumThread((hi + lo) / 2, hi, in, bitsum);
+					left.fork();
+					int[] rightAns = right.compute();
+					int[] leftAns = left.join();
+					
+					int[] out = new int[rightAns.length + leftAns.length];
+					for (int i = 0; i < leftAns.length; i++) {
+						out[i] = leftAns[i];
+					}
+					int j = 0;
+					for (int i = leftAns.length; i < out.length; i++) {
+						out[i] = rightAns[j];
+						j++;
+					}
+					return out;
+				}
+
+			}
+
+		}
+		SumThread thd = new SumThread(0, input.length, input, bitsum);
+		return ForkJoinPool.commonPool().invoke(thd);
+
+	}
+
+	private static void usage() {
+		System.err.println("USAGE: FilterEmpty <String array>");
+		System.exit(1);
+	}
+
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			usage();
+		}
+
+		String[] arr = args[0].replaceAll("\\s*", "").split(",");
+		System.out.println(Arrays.toString(filterEmpty(arr)));
+	}
 }
